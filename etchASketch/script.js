64c60d0184a584d69308ff238656568ad768a4bb
Rozie <rxiao1000@gmail.com>
2022-07-12 19:50:17 -0400

    Still need to associate the slider and grid. Make the warm and cool hover effects apply as well

--- etchASketch/script.js
+++ etchASketch/script.js
@@ -1,15 +1,3 @@
-// const grid = document.querySelector(".grid");
-// const btn = document.getElementById("create");
-
-// btn.addEventListener("click", function(e){
-//     const div = document.createElement("div");
-//     div.classList.add("tiles");
-//     console.log("hello")
-//     grid.appendChild(div);
-// });
-
-
-
 //fetching the range slider
 var slider = document.getElementById("range-slider");
 var output = document.getElementById("demo");
@@ -19,12 +7,16 @@ slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
+
+
 const btn = document.getElementById("create");
-const grid = document.querySelector(".grid");
+const btnWarm = document.getElementById("warm-RGB");
+const btnCool = document.getElementById("cool-RGB");
 
 
 function makeGrid(count){
 
+const grid = document.querySelector(".grid");
 
     // let width = Math.ceil(960 % count);
     grid.style.setProperty('--grid-rows', count);    
@@ -40,23 +32,9 @@ function makeGrid(count){
 // grid.style.cssText = ` grid-template-rows: repeat(${count}, ${width}fr);`
 }
 
-function getRangeVal(){
-    // console.log(slider.value);
-    return slider.value;
-}
-
-// const container = document.querySelector(".grid");
-
-// function makeRows(rows, cols) {
-//   container.style.setProperty('--grid-rows', rows);
-//   container.style.setProperty('--grid-cols', cols);
-//   for (c = 0; c < (rows * cols); c++) {
-//     let cell = document.createElement("div");
-//     cell.innerText = (c + 1);
-//     container.appendChild(cell).className = "grid-item";
-//   };
-// };
 
 
-
-btn.addEventListener("click", makeGrid(getRangeVal()));
+btn.addEventListener("click", makeGrid(slider.value));
+btnWarm.addEventListener("click", function(e){
+    
+});
