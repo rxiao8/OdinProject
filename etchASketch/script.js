0469a991ce1c59da6997c917d4125e6a0ed383a0
Rozie <rxiao1000@gmail.com>
2022-07-15 20:17:15 -0400

    fixed the buttons layout and added the RGB wheel. Need to make an interface for the color-pen functions

--- etchASketch/script.js
+++ etchASketch/script.js
@@ -7,8 +7,7 @@ slider.oninput = function () {
   output.innerHTML = `${this.value} x ${this.value}`;
 };
 
-
-window.addEventListener("load", ()=>{
+window.addEventListener("load", () => {
   output.innerHTML = "16 x 16";
 });
 
@@ -22,19 +21,18 @@ const btnClear = document.getElementById("clear");
 
 //creates the grid
 function makeGrid() {
-  
   const container = document.querySelector(".grid-container");
 
   var count = slider.value;
   // if(container.hasChildNodes()){
   //   container.removeChild(container.children[0]);
-  // }  
+  // }
   const grid = document.querySelector(".grid");
 
-  if(grid.hasChildNodes()){
+  if (grid.hasChildNodes()) {
     while (grid.firstChild) {
       grid.firstChild.remove();
-   }
+    }
   }
 
   grid.style.setProperty("--grid-rows", count);
@@ -43,19 +41,18 @@ function makeGrid() {
     const div = document.createElement("div");
     div.classList.add("grid-items");
     // div.textContent = i;
-    div.style.opacity = .1;
+    // div.style.opacity = .1;
+    div.style.backgroundClip = "padding-box";
     grid.appendChild(div);
   }
   // console.log(grid.children.length);
   // console.log(count);
   container.appendChild(grid);
-
 }
 
 //go over here
 btn.addEventListener("click", makeGrid);
 // makeGrid(slider.value);
-
 
 /*[document.querySelector('.a-class'), document.querySelector('.another-class')].forEach(item => {
   item.addEventListener('click', event => {
@@ -64,27 +61,53 @@ btn.addEventListener("click", makeGrid);
 })*/
 
 
+// function buttonActivation(button, action, type, color){
+//   const tiles = document.querySelectorAll(".grid-items");
+//   var temp = window.getComputedStyle(item).getPropertyValue("opacity");
+//   if(type === "canvas"){
+//     // button.addEventListener("load", (event)=>{
+//     //   tiles.forEach(funtion(item){
+//     //     item.addEventListener("mouseover", function(){
+//     //       var temp = window.getComputedStyle(item).getPropertyValue("opacity");
+//     //       temp = +temp + 0.1;
+//     //       item.style.opacity = temp;
+//     //       item.style.backgroundColor = color;
+//     //     });
+//     //   });
+//     // });
+//   }
+//   else{
+//     button.addEventListener("click", function (e) {
+//       // const tiles = document.querySelectorAll(".grid-items");
+//       // console.log("black");
+//       tiles.forEach(function (item) {
+//         // item.style.setProperty("--tile-color-hover", "red");
+//         item.addEventListener("mouseover", function () {
+//           temp = +temp + 0.1; //since temp was a string
+//           // temp =parseFloat(temp) + 0.1; //another way to parse
+//           item.style.opacity = temp;
+//           item.style.backgroundColor = color;
+//         });
+//       });
+//     });
+//   }
+// }
 //to activate black pen color when window loads
 window.addEventListener("load", (event) => {
   makeGrid(slider.value);
   const tiles = document.querySelectorAll(".grid-items");
   // console.log("Page Loaded");
   tiles.forEach(function (item) {
-
     item.addEventListener("mouseover", function () {
       var temp = window.getComputedStyle(item).getPropertyValue("opacity");
-      temp = +temp+ 0.1; //since temp was a string
+      temp = +temp + 0.1; //since temp was a string
       // temp =parseFloat(temp) + 0.1; //another way to parse
       item.style.opacity = temp;
       item.style.backgroundColor = "black";
-      console.log(window.getComputedStyle(item).getPropertyValue("opacity"));
-      // let numOpac = item.style.opacity.value;
-      // numOpac+= .1;
-      // item.style.opacity = numOpac;
+      // console.log(window.getComputedStyle(item).getPropertyValue("opacity"));
     });
   });
 });
-
 
 btn.addEventListener("click", (event) => {
   const tiles = document.querySelectorAll(".grid-items");
@@ -93,14 +116,13 @@ btn.addEventListener("click", (event) => {
     // item.style.setProperty("--tile-color-hover", "red");
     item.addEventListener("mouseover", function () {
       var temp = window.getComputedStyle(item).getPropertyValue("opacity");
-      temp = +temp+ 0.1; //since temp was a string
+      temp = +temp + 0.1; //since temp was a string
       // temp =parseFloat(temp) + 0.1; //another way to parse
       item.style.opacity = temp;
       item.style.backgroundColor = "black";
     });
   });
 });
-
 
 //changes pen color to black
 btnBlack.addEventListener("click", function (e) {
@@ -110,14 +132,13 @@ btnBlack.addEventListener("click", function (e) {
     // item.style.setProperty("--tile-color-hover", "red");
     item.addEventListener("mouseover", function () {
       var temp = window.getComputedStyle(item).getPropertyValue("opacity");
-      temp = +temp+ 0.1; //since temp was a string
+      temp = +temp + 0.1; //since temp was a string
       // temp =parseFloat(temp) + 0.1; //another way to parse
       item.style.opacity = temp;
       item.style.backgroundColor = "black";
     });
   });
 });
-
 
 //changes pen color to red
 btnWarm.addEventListener("click", function (e) {
@@ -127,7 +148,7 @@ btnWarm.addEventListener("click", function (e) {
     // item.style.setProperty("--tile-color-hover", "red");
     item.addEventListener("mouseover", function () {
       var temp = window.getComputedStyle(item).getPropertyValue("opacity");
-      temp = +temp+ 0.1; //since temp was a string
+      temp = +temp + 0.1; //since temp was a string
       // temp =parseFloat(temp) + 0.1; //another way to parse
       item.style.opacity = temp;
       item.style.backgroundColor = "red";
@@ -143,7 +164,7 @@ btnCool.addEventListener("click", function (e) {
     //  item.style.setProperty("--tile-color-hover", "blue");
     item.addEventListener("mouseover", function () {
       var temp = window.getComputedStyle(item).getPropertyValue("opacity");
-      temp = +temp+ 0.1; //since temp was a string
+      temp = +temp + 0.1; //since temp was a string
       // temp =parseFloat(temp) + 0.1; //another way to parse
       item.style.opacity = temp;
       item.style.backgroundColor = "blue";
@@ -157,7 +178,8 @@ btnErasor.addEventListener("click", function (e) {
   tiles.forEach(function (item) {
     //  item.style.setProperty("--tile-color-hover", "white");
     item.addEventListener("mouseover", function () {
-
+      var temp = window.getComputedStyle(item).getPropertyValue("opacity");
+      item.style.opacity = 0.1;
       item.style.backgroundColor = "white";
     });
   });
@@ -168,9 +190,9 @@ btnClear.addEventListener("click", (event) => {
   const tiles = document.querySelectorAll(".grid-items");
   tiles.forEach(function (item) {
     var temp = window.getComputedStyle(item).getPropertyValue("opacity");
-    temp = +temp+ 0.1; //since temp was a string
+    //since temp was a string
     // temp =parseFloat(temp) + 0.1; //another way to parse
-    item.style.opacity = temp;
+    item.style.opacity = 0.1;
     // item.style.setProperty("--tile-color-hover", "red");
     item.style.backgroundColor = "white";
   });
@@ -183,3 +205,28 @@ btnClear.addEventListener("click", (event) => {
 //     item.style.backgroundColor= "lightblue";
 //   });
 // })
+
+//allows users to pick from color wheel
+const rgbWheel = document.getElementById("RGB-wheel");
+rgbWheel.addEventListener("click", (event) => {
+  const colorVal = document.querySelector("#color-wheel-tiles").value;
+  const tiles = document.querySelectorAll(".grid-items");
+  tiles.forEach(function (item) {
+    //  item.style.setProperty("--tile-color-hover", "white");
+    item.addEventListener("mouseover", function () {
+      item.style.backgroundColor = colorVal;
+      // console.log(colorVal);
+    });
+  });
+});
+
+const btnBack = document.getElementById("background");
+btnBack.addEventListener("click", (event) => {
+  const colorVal = document.querySelector("#color-wheel-background").value;
+  const tiles = document.querySelectorAll(".grid-items");
+  tiles.forEach(function (item) {
+    //  item.style.setProperty("--tile-color-hover", "white");
+
+    item.style.backgroundColor = colorVal;
+  });
+});
