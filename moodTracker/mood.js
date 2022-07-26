c73801250bb2a3b234c092308dea2c54176558f8
Rozie <rxiao1000@gmail.com>
2022-07-25 20:09:17 -0400

    Started the mood tracker

--- moodTracker/mood.js
+++ moodTracker/mood.js
@@ -18,19 +18,29 @@
 
 // moodBtn.addEventListener("click", createTiles);
 
-
+const moodArr = ["amazing", "decent", "lazy", "stressed", "sad", "upset"];
 const moodBtn = document.querySelector(".moodBtn");
-moodBtn.addEventListener("click", function(e){
+window.addEventListener("load", function(e){
     console.log("here");
     const mood = document.querySelector(".mood-range");
     // mood.textContent = "world0";
-    mood.style.setProperty("--grid-cols", 6);
+    mood.style.setProperty("--grid-rows", 6);
     for (var i = 0; i < 7; i++) {
             const div = document.createElement("div");
-        //     div.classList.add("tile");
+            div.classList.add("tile");
             div.style.backgroundClip = "border-box";
             div.style.padding = "10px";
+            div.textContent = moodArr[i];
             // div.textContent = i;
             mood.appendChild(div);
           }
 })
+
+//using js to create a gradient for the tracker
+
+// const moodList = document.getElementsByClassName("tiles");
+// moodList.array.forEach(element => {
+//     var color = element.style.color.value;
+//     const colorArr = color.slice_
+//     element.style.color
+// });
