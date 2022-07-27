b3a498dd8b73ea2a68e600b548918266d33727e5
Rozie <rxiao1000@gmail.com>
2022-07-27 18:39:48 -0400

    Figured out how to add a new row. Need to know how to update file management based on button click.

--- moodTracker/script.js
+++ moodTracker/script.js
@@ -1,5 +1,11 @@
 // //import the xlsx pkg
-var xlsx = require("xlsx");
+// var xlsx = require("xlsx");
+
+// import xlsx from "xlsx";
+// import * as XLSX from 'xlsx/xlsx.mjs';
+const fs = require("fs");
+// const xlsx = 
+
 // // //grab the wb
 // var wb = xlsx.readFile("xlsx/Financial Sample.xlsx",{cellDates: true});
 // //grab a sheet of the wb
@@ -29,15 +35,43 @@ var xlsx = require("xlsx");
 // console.log(ws2);
 
 
+//fetch the corresponding btns
+const weightInput = document.getElementById("weight-input");
+const waistInput = document.getElementById("waist-input");
+const btn = document.querySelector(".weightBtn");
 
-var wbWeight = xlsx.readFile("xlsx/WeightBook.xlsx", {cellDates: true});
+ function addWeight(){
+//read in the ws
+var wbWeight = xlsx.readFile("workBook/WeightBook.xlsx", {cellDates: true});
+//grab the desired ws
 var wsWeight = wbWeight.Sheets["Weight"];
+//convert the data into an arr of json
 const dataWeight = xlsx.utils.sheet_to_json(wsWeight);
-dataWeight[0] = [["hflad", "hfaldkjf", "hfladkjf"]];
-// wsWeight.utils.json_to_sheet(dataWeight);
-const newWS = xlsx.utils.json_to_sheet(dataWeight);
-xlsx.utils.book_append_sheet(wbWeight, newWS, "Weight");
-// console.log(dataWeight[0]);
+console.log(dataWeight);
+
+//input the curr data,, weight,, and waist (opt)
+var today2 = new Date();
+var dd2 = String(today2.getDate());
+var mm2 = String(today2.getMonth() + 1); //January is 0!
+var yyyy2 = today2.getFullYear();
+
+today2 = mm + '/' + dd + '/' + yyyy;
+
+//adds data to a new row
+xlsx.utils.sheet_add_aoa(wsWeight, [
+    [today2, weightInput.value, waistInput.value]
+  ], {origin: -1});
+
+//writes the new ws to the same wb
+xlsx.writeFile(wbWeight,"xlsx/WeightBook.xlsx" );
+}
+
+
+btn.addEventListener("click", addWeight);
+// // wsWeight.utils.json_to_sheet(dataWeight);
+// const newWS = xlsx.utils.json_to_sheet(dataWeight);
+// xlsx.utils.book_append_sheet(wbWeight, newWS, "Weight");
+// console.log(dataWeight[0]);i
 
 // const weightBtn = document.querySelector(".weightBtn");
 // const waistBtn = document.querySelector(".waistBtn");
